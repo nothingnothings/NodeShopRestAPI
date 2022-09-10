a9f9154072cfcd414a9f1d18fd75c4d50c27c1f2
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-09-10 17:25:18 -0300

    changed placeholder text in input fields

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -24,7 +24,7 @@ const Auth = (props) => {
       inputType: 'input',
       elementConfig: {
         type: 'email',
-        placeholder: 'exemplo@exemplo',
+        placeholder: 'exemplo@exemplo.com',
       },
       value: '',
       validation: {
@@ -73,7 +73,7 @@ const Auth = (props) => {
           inputType: 'input',
           elementConfig: {
             type: 'email',
-            placeholder: 'exemplo@exemplo',
+            placeholder: 'exemplo@exemplo.com',
           },
           value: '',
           validation: {
